58460dc4efd707624bc93641d0cce3b493febf40
nikoe-ee <nicholas.tantan2009@gmail.com>
2026-07-22 14:01:07 +0700

    feat: wip all

---
+++ app/Http/Controllers/MajorClass/create.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\MajorClass;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class create extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/students', [StudentsController::class, 'index'])
+->name('students.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,8 +15,34 @@ Route::get('/', function () {
 route::name('students.')->prefix('students')->group(function() {
         Route::get('/', [StudentsController::class, 'index'])->name('index');
 
+// Halaman Detail Siswa
 Route::get('/{id}', function($id){
     return "Menampilkan detail siswa dengan ID = {$id}";
 })->name('show');
+
+// Halaman Tambah Siswa
+Route::get('/create', function(){
+    return "Ini adalah halaman tambah siswa ";
+})->name('create');
+
+// Halaman Edit Siswa
+Route::get('/students/{id}/edit', function(string $id){
+    return "Ini adalah halaman edit siswa dengan ID: {$id}";
+})->name('edit');
+
+// Logika Tambah Siswa
+Route::post('/students', function() {
+    return "Menambah data siswa baru";
+})->name('store');
+
+// Logika Edit Siswa
+Route::put('/students/{id}', function(string $id) {
+    return "Mengubah data siswa dengan ID: {$id}";
+})->name('update');
+
+// Logika Hapus Siswa
+Route::delete('/students/{id}', function(string $id) {
+    return "menghapus data siswa dengan ID: {$id}";
+})->name('destroy');
 
 });
